9bac5ee1912f6349f2963c9d160d66d19e224574
stabassum <sumu1993@gmail.com>
2021-05-20 16:09:37 -0400

    commit tree 7ce150045775dbd13d62e10e76109289b883fa9c

--- src/App.js
+++ src/App.js
@@ -9,18 +9,11 @@ class App extends Component {
     timerIDs: []
   }
 
-
   //Your code here:
 
   componentDidMount(){
     this.handleAddTimer()
   }
-
-
-
-
-
-
 
   // No need to modify anything in render or the class methods below
   // Unless, of course, you're curious about how it all works
@@ -57,7 +50,6 @@ class App extends Component {
       timerIDs: prevState.timerIDs.filter(timer_id => timer_id !== id)
     }))
   }
-
 
 }
 
